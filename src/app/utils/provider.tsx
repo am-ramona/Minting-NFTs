9322b18cfe9ceb1ca584d392cd6dc7d799d933d6
am-ramona <ramona.abimoussa@gmail.com>
2024-01-30 23:09:42 +0200

    re-adjusted the type of the children props for Provider

--- src/app/utils/provider.tsx
+++ src/app/utils/provider.tsx
@@ -3,7 +3,7 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react"
 import { Goerli } from "@thirdweb-dev/chains"
 
-const Provider = ({children: any}) => {
+const Provider = ({children}: any) => {
     return(
     <ThirdwebProvider activeChain={Goerli} ChainId={process.env.NEXT_PUBLIC_CHAIN_ID} clientId="bf883a55580f9e31c1cbaa9a3e3996ef">
         {children}
