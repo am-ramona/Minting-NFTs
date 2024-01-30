f8da2a8be635c9bf01607842fe33f746c760c7de
am-ramona <ramona.abimoussa@gmail.com>
2024-01-30 23:20:53 +0200

    Removed ChainId from ThirdwebProvider

--- src/app/utils/provider.tsx
+++ src/app/utils/provider.tsx
@@ -5,7 +5,7 @@ import { Goerli } from "@thirdweb-dev/chains"
 
 const Provider = ({children}: any) => {
     return(
-    <ThirdwebProvider activeChain={Goerli} ChainId={process.env.NEXT_PUBLIC_CHAIN_ID} clientId="bf883a55580f9e31c1cbaa9a3e3996ef">
+    <ThirdwebProvider activeChain={Goerli} clientId="bf883a55580f9e31c1cbaa9a3e3996ef">
         {children}
     </ThirdwebProvider>
     )
