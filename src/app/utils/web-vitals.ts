78f6b74de05c623c7d92e06f1b93c168e5d754d1
am-ramona <ramona.abimoussa@gmail.com>
2024-06-26 17:42:30 +0300

    Added a type to web-vital

--- src/app/utils/web-vitals.ts
+++ src/app/utils/web-vitals.ts
@@ -1,9 +1,26 @@
 'use client'
 
-import { useReportWebVitals } from 'next/web-vitals'
+import { useReportWebVitals, ReportHandler } from 'next/web-vitals'
  
-export function WebVitals() {
+const WebVitals = () => {
   useReportWebVitals((metric) => {
     console.log(metric)
   })
 }
+
+export default WebVitals
+// import { ReportHandler } from 'web-vitals';
+
+// const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
